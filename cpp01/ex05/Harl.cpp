c1c723538f5b31757f7e837475c64285c1d57951
Mikael Belmont <92719243+mikaelbelmont@users.noreply.github.com>
2023-11-17 17:49:36 +0000

    Add files via upload

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,11 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarreto <mbarreto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 15:29:29 by mbarreto          #+#    #+#             */
+/*   Updated: 2023/11/15 16:38:07 by mbarreto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
 void Harl::debug(void)
 {
-	std::cout << "I love ";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 Harl::Harl(void)
 {
-	std::cout << 
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "Default destructor called" << std::endl;
 }
